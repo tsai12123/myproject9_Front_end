c9caebef4e7a8f1dc1418c531eb30b49fab97db7
bo chen <bochen@gmail.com>
2023-10-06 19:55:57 +0800

    updata button

--- src/components/home-component.js
+++ src/components/home-component.js
@@ -13,7 +13,12 @@ const HomeComponent = () => {
               項目，它是創建現代網站的最流行的方式之一。
             </p>
             <button className="btn btn-primary btn-lg" type="button">
-              看看它怎麼運作。
+              <a
+                href="/login"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                看看它怎麼運作。
+              </a>
             </button>
           </div>
         </div>
@@ -25,8 +30,13 @@ const HomeComponent = () => {
               <p>
                 學生可以註冊他們喜歡的課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
               </p>
-              <button className="btn btn-outline-light" type="button">
-                登錄會員、或者註冊一個帳號
+              <button className="btn btn-secondary" type="button">
+                <a
+                  href="/login"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  登錄會員、或者註冊一個帳號
+                </a>
               </button>
             </div>
           </div>
